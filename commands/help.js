f3fa3010acfb33fa56a0807eb8eeeb736b70578b
Cryanide <addtext@hotmail.com>
2017-08-04 16:09:21 -0400

    edited help command

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
 exports.run = (client, message, args) => {
-  
+  const embed = new Discord.RichEmbed()
+  .setDescription('[Commands & easy link to Github](https://github.com/Cryanide/MayaBot/blob/master/Help%20txt)\n[just the commands](https://raw.githubusercontent.com/Cryanide/MayaBot/master/Help%20txt)')
+  message.channel.send({ embed })
 }
